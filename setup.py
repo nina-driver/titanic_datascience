d1eb986c3961f05800e71f8a7592bff94c20fc95
nina-driver <driver.nina@gmail.com>
2020-04-18 22:07:06 -0700

    Refactor exploratory analysis of passenger survival predictions using ridge logistic regression

--- setup.py
+++ setup.py
@@ -25,4 +25,16 @@ setup(
     author_email='driver.nina@gmail.com',  # Substitute your email
     license='MIT',
     packages=['titanic'],
+    install_requires=[
+        'pypandoc>=1.4',
+        'watermark>=1.8.1',
+        'pandas>=0.24.2',
+        'scikit-learn>=0.20.3',
+        'scipy>=1.2.1',
+        'matplotlib>=3.0.3',
+        'pytest>=4.3.1',
+        'pytest-runner>=4.4'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
 )
